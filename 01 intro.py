82986cc5f7a9f0af3d9af02a2306b9b44d40f6a6
Arctaea <joycacao@gmail.com>
2024-11-07 21:28:42 +0800

    Introduction to Python from datacamp python basics, lists, functions and packages, NumPy

---
+++ 01 intro.py
@@ -0,0 +1,19 @@
+# Basic Operations
+fir=8
+sec=4
+
+# Simple
+# print("Addition: ", str(fir) + " + " + str(sec) + " = " + str(fir+sec))
+
+# String literals formatting
+# Addition
+print(f"Addition: {fir} + {sec} = {fir+sec}")
+
+# Subtraction
+print(f"Subtraction: {fir} - {sec} = {fir-sec}")
+
+# Multiplication
+print(f"Multiplication: {fir} * {sec} = {fir*sec}")
+
+# Division
+print(f"Division: {fir} / {sec} = {fir/sec}")
---
+++ 10 list update.py
@@ -0,0 +1,9 @@
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+print(areas)
+# Correct the bathroom area
+areas[-1]=10.50
+print(areas)
+# Change "living room" to "chill zone"
+areas[4]="chill zone"
+print(areas)

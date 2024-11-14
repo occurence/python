1a090df95ff5c0e0376af1b734b0d5d5fd4711cf
Arctaea <joycacao@gmail.com>
2024-11-14 19:51:17 +0800

    Data Manipulation with pandas dataframe methods, attributes, sorting, and subsetting

---
+++ Course_Data_Manipulation_Pandas/01 df methods attributes.py
@@ -0,0 +1,15 @@
+import pandas as pd
+homelessness = pd.read_csv(r'D:\STUDY\python\Course_Data_Manipulation_Pandas\homelessness.csv')
+
+
+# Print the head of the homelessness data
+print(homelessness.head())
+
+# Print information about homelessness
+print(homelessness.info())
+
+# Print the shape of homelessness
+print(homelessness.shape)
+
+# Print a description of homelessness
+print(homelessness.describe())
---
+++ Course_Data_Manipulation_Pandas/04 df subset.py
@@ -0,0 +1,17 @@
+import pandas as pd
+homelessness = pd.read_csv(r'D:\STUDY\python\Course_Data_Manipulation_Pandas\homelessness.csv')
+
+# Select the individuals column
+individuals = homelessness['individuals']
+
+print(individuals.head())
+
+# Select the state and family_members columns
+state_fam = homelessness[['state', 'family_members']]
+
+print(state_fam.head())
+
+# Select only the individuals and state columns, in that order
+ind_state = homelessness[['individuals','state']]
+
+print(ind_state.head())

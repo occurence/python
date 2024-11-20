0730b5c63e1392f1b9190ca496049a55df2aecea
Arctaea <joycacao@gmail.com>
2024-11-20 19:23:52 +0800

    Data Manipulation with pandas Create and Visualize DataFrames: groupby plot bar,line,scatter,histogram

---
+++ Course_Data_Manipulation_Pandas/35 df plot avocado type.py
@@ -0,0 +1,15 @@
+import pandas as pd
+avocados = pd.read_pickle(r'D:\STUDY\python\Course_Data_Manipulation_Pandas\avoplotto.pkl')
+import matplotlib.pyplot as plt
+
+# Modify bins to 20
+avocados[avocados["type"] == "conventional"]["avg_price"].hist(bins=20, alpha=0.5)
+
+# Modify bins to 20
+avocados[avocados["type"] == "organic"]["avg_price"].hist(bins=20, alpha=0.5)
+
+# Add a legend
+plt.legend(["conventional", "organic"])
+
+# Show the plot
+plt.show()

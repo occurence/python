5c4fb26f236474d2fe147866dac4abb26cf723b0
Arctaea <joycacao@gmail.com>
2024-11-22 06:48:49 +0800

    Project: Exploring NYC Public School Test Result Scores df sort, top rows, groupby, aggregate, rename columns, indexing

---
+++ Project_Exploring_School_Result/project02.py
@@ -0,0 +1,28 @@
+# Re-run this cell 
+import pandas as pd
+
+# Read in the data
+schools = pd.read_csv(r"Project_Exploring_School_Result\schools.csv")
+
+# Preview the data
+# print(schools.head())
+
+# Start coding here...
+# Add as many cells as you like...
+
+# best_math_schools = schools[['school_name','average_math']].sort_values(['school_name','average_math'], ascending=[True,False])
+best_math_schools = schools[schools["average_math"] >= 640][['school_name','average_math']].sort_values(['school_name','average_math'], ascending=[True,False])
+print(best_math_schools)
+
+schools['total_SAT'] = schools['average_math'] + schools['average_reading'] + schools['average_writing']
+top_10_schools = schools[['school_name','total_SAT']].sort_values('total_SAT', ascending=False)
+print(top_10_schools)
+
+schools_borough = schools.groupby('borough')['total_SAT'].std().reset_index(name='total_SAT')
+max_borough = schools_borough.loc[schools_borough['total_SAT'].idxmax(), 'borough']
+# print(max_borough)
+
+
+largest_std_dev = max_borough,schools.groupby('borough')['total_SAT'].max().max(),schools['total_SAT'].mean(),schools['total_SAT'].std()
+
+print(largest_std_dev)

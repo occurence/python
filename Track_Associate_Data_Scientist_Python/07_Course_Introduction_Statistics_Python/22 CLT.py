d0209611cab4485267636f189f526a706a6a3b7e
Arctaea <joycacao@gmail.com>
2024-11-26 20:14:24 +0800

    Introduction to Statistics in Python Distributions and CLT: statistics distribution, [normal, binomial, uniform, poisson, exponential, students t], [cdf, ppf, pmf, rvs]

---
+++ Track_Associate_Data_Scientist_Python/07_Course_Introduction_Statistics_Python/22 CLT.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+amir_deals = pd.read_csv(r'D:\STUDY\python\Track_Associate_Data_Scientist_Python\07_Course_Introduction_Statistics_Python\datasets\amir_deals.csv')
+
+# Create a histogram of num_users and show
+amir_deals['num_users'].hist()
+plt.show()
+
+# Set seed to 104
+np.random.seed(104)
+
+# Sample 20 num_users with replacement from amir_deals
+samp_20 = amir_deals['num_users'].sample(20, replace=True)
+
+# Take mean of samp_20
+print(samp_20.mean())
+
+# Sample 20 num_users with replacement from amir_deals and take mean
+samp_20 = amir_deals['num_users'].sample(20, replace=True)
+np.mean(samp_20)
+
+sample_means = []
+# Loop 100 times
+for i in range(100):
+  # Take sample of 20 num_users
+  samp_20 = amir_deals['num_users'].sample(20, replace=True)
+  # Calculate mean of samp_20
+  samp_20_mean = np.mean(samp_20)
+  # Append samp_20_mean to sample_means
+  sample_means.append(samp_20_mean)
+  
+print(sample_means)
+
+# Convert to Series and plot histogram
+sample_means_series = pd.Series(sample_means)
+sample_means_series.hist()
+# Show plot
+plt.show()

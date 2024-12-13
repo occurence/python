ed3f5034d17e5c024dca6f8763fb4e44c38d6321
Arctaea <joycacao@gmail.com>
2024-12-13 19:54:28 +0800

    Cleaning Data Text and Categorical Data Problems: whitespace and capitalization inconsistencies in category labels, collapse categories into one, reformat and remap string for consistency

---
+++ Track_Associate_Data_Scientist_Python/20_Course_Cleaning_Data_in_Python/08 inconsistent categorical variables.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+airlines = pd.read_csv(r'D:\STUDY\python\Track_Associate_Data_Scientist_Python\20_Course_Cleaning_Data_in_Python\datasets\airlines.csv')
+
+# Print unique values of both columns
+print(airlines['dest_region'].unique())
+print(airlines['dest_size'].unique())
+
+# Lower dest_region column and then replace "eur" with "europe"
+airlines['dest_region'] = airlines['dest_region'].str.lower() 
+airlines['dest_region'] = airlines['dest_region'].replace({'eur':'europe'})
+
+# Remove white spaces from `dest_size`
+airlines['dest_size'] = airlines['dest_size'].str.strip()
+
+# Verify changes have been effected
+print(airlines['dest_region'].unique())
+print(airlines['dest_size'].unique())

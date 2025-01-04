8687f674bd6fdc640a3df71d9d0d12fd86bf57b7
Arctaea <joycacao@gmail.com>
2025-01-05 06:05:58 +0800

    Hypothesis Testing Hypothesis Testing Fundamentals: hypothesized proportion, std_error, z_score, p_value, confidence interval quantiles

--- Track_Associate_Data_Scientist_Python/29_Course_Hypothesis_Testing_in_Python/01 sample mean.py
+++ Track_Associate_Data_Scientist_Python/29_Course_Hypothesis_Testing_in_Python/01 sample mean.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-late_shipments = pd.read_csv(r'D:\STUDY\python\Track_Associate_Data_Scientist_Python\28_Course_Sampling_in_Python\datasets\late_shipments.csv')
+late_shipments = pd.read_csv(r'D:\STUDY\python\Track_Associate_Data_Scientist_Python\29_Course_Hypothesis_Testing_in_Python\datasets\late_shipments.csv')
 
 # Print the late_shipments dataset
 print(late_shipments)
--- Track_Associate_Data_Scientist_Python/29_Course_Hypothesis_Testing_in_Python/03 p-values.py
+++ Track_Associate_Data_Scientist_Python/29_Course_Hypothesis_Testing_in_Python/03 p-values.py
@@ -1,26 +1,22 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
 
 late_shipments = pd.read_csv(r'D:\STUDY\python\Track_Associate_Data_Scientist_Python\29_Course_Hypothesis_Testing_in_Python\datasets\late_shipments.csv')
 late_prop_samp = (late_shipments['late'] == 'Yes').mean()
-
 late_shipments_boot_distn = []
 for i in range(5000):
     late_shipments_boot_distn.append(
     	np.mean(late_shipments.sample(frac=1, replace=True)['late_delivery'])
     )
-
-print(late_shipments_boot_distn)
-print(late_prop_samp)
-
-# Hypothesize that the proportion is 6%
 late_prop_hyp = 0.06
-
-# Calculate the standard error
 std_error = np.std(late_shipments_boot_distn, ddof=1)
 
-# Find z-score of late_prop_samp
-z_score = (late_prop_samp - late_prop_hyp) / std_error
+# Calculate the z-score of late_prop_samp
+z_score = (late_prop_samp-late_prop_hyp)/std_error
 
-# Print z_score
-print(z_score)
+# Calculate the p-value
+p_value = 1 - norm.cdf(z_score, loc=0, scale=1)
+                 
+# Print the p-value
+print(p_value) 

504f68ce537bb11fd5dbd8fae6c782c2b85de60c
Arctaea <joycacao@gmail.com>
2025-01-09 23:52:51 +0800

    Hypothesis Testing Proportion Tests: test for single proportions

---
+++ Track_Associate_Data_Scientist_Python/29_Course_Hypothesis_Testing_in_Python/14 test single proportions.py
@@ -0,0 +1,11 @@
+# Hypothesize that the proportion of late shipments is 6%
+p_0 = ____
+
+# Calculate the sample proportion of late shipments
+p_hat = ____
+
+# Calculate the sample size
+n = ____
+
+# Print p_hat and n
+print(p_hat, n)

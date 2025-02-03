7f63a05cd97f42552c3b704f6c180fcd19165beb
Arctaea <joycacao@gmail.com>
2025-02-03 18:53:42 +0800

    Introduction to SQL Server SELECTion Box: ordering and filtering(order by desc/asc, where text, numeric, date, between range, is null, is not null)

---
+++ Track_SQL_Server_Developer/01_Course_Introduction_to_SQL_Server/15 null values.py
@@ -0,0 +1,24 @@
+import pyodbc
+import pandas as pd
+
+conn = pyodbc.connect(
+    "DRIVER={SQL Server};"
+    "SERVER=Arc-PC;"
+    "DATABASE=datacamp;"
+    "Trusted_Connection=True;"
+)
+
+query = """
+-- Retrieve all columns
+SELECT 
+  * 
+FROM 
+  grid 
+  -- Return only rows where demand_loss_mw is missing or unknown  
+WHERE 
+  demand_loss_mw IS NULL;
+"""
+
+df = pd.read_sql(query, conn)
+print(df)
+conn.close()

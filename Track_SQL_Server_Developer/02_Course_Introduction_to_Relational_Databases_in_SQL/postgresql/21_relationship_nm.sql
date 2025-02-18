b8a1ab5a02ed459431d518c608ebd16df293e89c
Arctaea <joycacao@gmail.com>
2025-02-18 18:18:59 +0800

    Introduction to Relational Databases in SQL Related Tables: many to many relationship and referenced column check postgresql query

--- Track_SQL_Server_Developer/02_Course_Introduction_to_Relational_Databases_in_SQL/postgresql/21_relationship_nm.sql
+++ Track_SQL_Server_Developer/02_Course_Introduction_to_Relational_Databases_in_SQL/postgresql/21_relationship_nm.sql
@@ -8,4 +8,15 @@ RENAME organization TO organization_id;
 
 -- Add a foreign key on organization_id
 ALTER TABLE affiliations
-ADD CONSTRAINT affiliations_organization_fkey FOREIGN KEY (organization_id) REFERENCES organizations (id);
+ADD CONSTRAINT affiliations_organization_id_fkey FOREIGN KEY (organization_id) REFERENCES organizations (id);
+
+SELECT 
+    tc.table_name,
+    tc.constraint_name,
+    tc.constraint_type,
+    kcu.column_name
+FROM information_schema.table_constraints AS tc
+LEFT JOIN information_schema.key_column_usage AS kcu
+    ON tc.constraint_name = kcu.constraint_name
+    AND tc.table_name = kcu.table_name
+WHERE tc.table_name IN ('affiliations');
--- Track_SQL_Server_Developer/02_Course_Introduction_to_Relational_Databases_in_SQL/postgresql/23_remove_reference_columns.sql
+++ Track_SQL_Server_Developer/02_Course_Introduction_to_Relational_Databases_in_SQL/postgresql/23_remove_reference_columns.sql
@@ -5,3 +5,8 @@ DROP COLUMN firstname;
 -- Drop the lastname column
 ALTER TABLE affiliations
 DROP COLUMN lastname;
+
+-- Query the right table in information_schema to get columns
+SELECT column_name, data_type 
+FROM information_schema.columns 
+WHERE table_name = 'affiliations';

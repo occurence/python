9cf2988c141a8f0887c91f1cf2579d5faa0f4b28
Arctaea <joycacao@gmail.com>
2025-02-19 18:00:59 +0800

    Intermediate SQL Server Summarizing Data: aggregate, missing values(impute, replace coalesce), case and groupings

---
+++ Track_SQL_Server_Developer/03_Course_Intermediate_SQL_Server/06 case.py
@@ -0,0 +1,33 @@
+import psycopg2
+import pandas as pd
+
+DB_PARAMS = { "dbname": "datacamp", "user": "postgres", "password": "postgres", "host": "localhost", "port": "5432", }
+
+try:
+    conn = psycopg2.connect(**DB_PARAMS)
+    cur = conn.cursor()
+
+    sql_script = """
+    """
+
+    info_query = """
+    SELECT Country, 
+        CASE WHEN Country = 'us'  THEN 'USA'
+        ELSE 'International'
+        END AS SourceCountry
+    FROM Incidents
+    """
+
+    # cur.execute(sql_script)
+    # conn.commit()
+
+    cur.execute(info_query)
+    
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    conn.close()
+
+except psycopg2.Error as e:
+    print(f"Error: {e}")

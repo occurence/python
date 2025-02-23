272762efa76466ee108f5a71b53928eb831d70ab
Arctaea <joycacao@gmail.com>
2025-02-23 18:06:48 +0800

    Cluster Analysis Introduction to Clustering: unsupervised learning algo techniques(hierarchical and kmeans) and preprocessing normalize/scaling data

---
+++ Track_Machine_Learning_Scientist_in_Python/09_Course_Cluster_Analysis_in_Python/03 kmeans cluster.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+x = [9, 6, 2, 3, 1, 7, 1, 6, 1, 7, 23, 26, 25, 23, 21, 23, 23, 20, 30, 23]
+y = [8, 4, 10, 6, 0, 4, 10, 10, 6, 1, 29, 25, 30, 29, 29, 30, 25, 27, 26, 30]
+
+df = pd.DataFrame({'x':x, 'y':y})
+df = df.astype(float)
+
+# Import kmeans and vq functions
+from scipy.cluster.vq import kmeans, vq
+
+# Compute cluster centers
+centroids,_ = kmeans(df, 2)
+
+# Assign cluster labels
+df['cluster_labels'], _ = vq(df, centroids)
+
+# Plot the points with seaborn
+sns.scatterplot(x='x', y='y', hue='cluster_labels', data=df)
+plt.show()

94dfdbbafe023e70a35986f3c264f441c8e4fc4c
Arctaea <joycacao@gmail.com>
2025-02-24 17:49:49 +0800

    Cluster Analysis Hierarchical Clustering: fcluster and linkage for(ward, single, complete methods), visuals(matplotlib, seaborn, dendrogram), timeit, and fifa samples

---
+++ Track_Machine_Learning_Scientist_in_Python/09_Course_Cluster_Analysis_in_Python/08 cluster hierarchical ward.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+comic_con = pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\09_Course_Cluster_Analysis_in_Python\datasets\comic_con.csv')
+
+# Import the fcluster and linkage functions
+from scipy.cluster.hierarchy import fcluster, linkage
+
+# Use the linkage() function
+distance_matrix = linkage(comic_con[['x_scaled', 'y_scaled']], method = 'ward', metric = 'euclidean')
+
+# Assign cluster labels
+comic_con['cluster_labels'] = fcluster(distance_matrix, 2, criterion='maxclust')
+
+# Plot clusters
+sns.scatterplot(x='x_scaled', y='y_scaled', 
+                hue='cluster_labels', data = comic_con)
+plt.show()

26390c0b873a6efcbea877da0265ab94ff6fa251
Arctaea <joycacao@gmail.com>
2025-03-12 16:18:08 +0800

    SSMS database master script and list file script

---
+++ Track_SQL_Server_Developer/06_Course_Database_Design/00 MASTER SCRIPT.py
@@ -0,0 +1,27 @@
+import runpy
+import os
+
+scripts_to_run = [
+    "01 schema migrate runs.py",
+]
+
+SCRIPT_DIR = "D:\\STUDY\\python\\Track_SQL_Server_Developer\\06_Course_Database_Design\\"
+
+# Execute each script in the list
+for script in scripts_to_run:
+    script_path = os.path.join(SCRIPT_DIR, script)
+    print(f"Executing: {script_path}")
+    
+    try:
+        runpy.run_path(script_path)
+    except Exception as e:
+        print(f"Error in {script}: {e}")
+
+"""
+import os
+extract_path = r"D:\STUDY\python\Track_SQL_Server_Developer\06_Course_Database_Design"
+os.makedirs(extract_path, exist_ok=True)
+import os
+extracted_files = os.listdir(extract_path)
+print(",\n".join(f'    "{file}"' for file in extracted_files))
+"""

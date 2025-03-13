7233f72b50512f80e30535a34f93889cbfd092e7
Arctaea <joycacao@gmail.com>
2025-03-13 23:30:23 +0800

    SSMS database SSMS schema scripts

--- Track_SQL_Server_Developer/06_Course_Database_Design/SSMS/02 schema tables route_dim.sql
+++ Track_SQL_Server_Developer/06_Course_Database_Design/SSMS/02 schema tables route_dim.sql
@@ -13,17 +13,7 @@ CREATE TABLE route_dim(
     distance_km FLOAT NOT NULL,
     route_name VARCHAR(160) NOT NULL
 );
--- Create a week dimension table
-DROP TABLE IF EXISTS week_dim;
-CREATE TABLE week_dim(
-	week_id INTEGER PRIMARY KEY,
-    week INTEGER NOT NULL,
-    month VARCHAR(160) NOT NULL,
-    year INTEGER NOT NULL
-);
 
--- Create a runs fact table
-DROP TABLE IF EXISTS runs_fact;
 
 WITH DistinctRuns AS (
     SELECT DISTINCT
@@ -52,32 +42,3 @@ SELECT
 FROM RowNumbers;
 
 SELECT * FROM route_dim;
-
-WITH DistinctWeeks AS (
-	SELECT
-	DISTINCT week, month, year,
-	MONTH(CAST('1-' + month + '-' + CAST(year AS VARCHAR(4)) AS DATETIME)) AS MonthOrder
-	FROM runs
-)
-, RowNumbers AS (
-    SELECT
-        ROW_NUMBER() OVER (ORDER BY MonthOrder) + 600 AS week_id,
-        week,
-        month,
-        year,
-		MonthOrder
-    FROM DistinctWeeks
-)
-INSERT INTO week_dim (week_id, week, month, year)
-SELECT week_id, week, month, year FROM RowNumbers;
-
-SELECT * FROM week_dim;
-
-SELECT
-	duration_mins, week_id, route_dim.route_id
-INTO runs_fact
-FROM runs
-INNER JOIN route_dim ON route_dim.route_name = runs.route_name
-INNER JOIN week_dim ON CONCAT(week_dim.week, '-', week_dim.month) = CONCAT(runs.week, '-', runs.month);
-
-SELECT * FROM runs_fact;

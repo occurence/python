53d8fe652ba2b16a4a4caf79c7ec41498b6e8343
Arctaea <joycacao@gmail.com>
2025-03-14 11:42:40 +0800

    SSMS database master script update

--- Track_SQL_Server_Developer/06_Course_Database_Design/00 MASTER SCRIPT.py
+++ Track_SQL_Server_Developer/06_Course_Database_Design/00 MASTER SCRIPT.py
@@ -2,7 +2,29 @@ import runpy
 import os
 
 scripts_to_run = [
+    # "00 LIST FILES.py",
     "01 schema migrate runs.py",
+    "02 schema generate route_dim.py",
+    "03 schema generate week_dim.py",
+    "04 schema generate runs_fact.py",
+    "05 query dimension.py",
+    "06 schema migrate fact_booksale.py",
+    "07 alter dimension star fk.py",
+    "08 schema migrate book.py",
+    "09 schema migrate time.py",
+    "10 schema migrate store.py",
+    "11 crud dimension snowflake book.py",
+    # "12 join star.py",
+    "13 schema migrate snowflake book.py",
+    "14 schema migrate snowflake author.py",
+    "15 schema migrate snowflake publisher.py",
+    "16 schema migrate snowflake genre.py",
+    "17 schema migrate snowflake store.py",
+    "18 schema migrate snowflake city.py",
+    "19 schema migrate snowflake state.py",
+    "20 schema migrate snowflake country.py",
+    # "21 join snowflake.py",
+    "22 schema migrate snowflake continent.py",
 ]
 
 SCRIPT_DIR = "D:\\STUDY\\python\\Track_SQL_Server_Developer\\06_Course_Database_Design\\"

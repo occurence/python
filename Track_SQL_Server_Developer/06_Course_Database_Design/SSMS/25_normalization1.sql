f2c17d88fc2f3502ce61cd6ea1029b5133dd104e
Arctaea <joycacao@gmail.com>
2025-03-16 21:59:48 +0800

    Database Design Database Schemas and Normalization: SSMS queries converted from postgresql

---
+++ Track_SQL_Server_Developer/06_Course_Database_Design/SSMS/25_normalization1.sql
@@ -0,0 +1,12 @@
+-- Create a new table to hold the cars rented by customers
+DROP TABLE IF EXISTS cust_rentals;
+CREATE TABLE cust_rentals (
+  customer_id INT NOT NULL,
+  car_id VARCHAR(128) NULL,
+  invoice_id VARCHAR(128) NULL
+);
+
+-- Drop two columns from customers table to satisfy 1NF
+ALTER TABLE customers
+DROP COLUMN cars_rented,
+invoice_id;

2b948d2330130afc5b5060a207e3c7bcb9a0690a
Arctaea <joycacao@gmail.com>
2025-03-17 08:05:25 +0800

    Machine Learning for Time Series Data Time Series and Machine Learning Primer: classification model linearSVC, regression model LinearRegression for(plot, fit, predict, and inspect)

---
+++ Track_Machine_Learning_Scientist_in_Python/12_Course_Machine_Learning_for_Time_Series_Data_in_Python/04 classification model linearSVC predict.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+iris = pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\12_Course_Machine_Learning_for_Time_Series_Data_in_Python\datasets\iris.csv')
+targets = pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\12_Course_Machine_Learning_for_Time_Series_Data_in_Python\datasets\targets.csv')
+
+from sklearn.svm import LinearSVC
+
+X = iris[['petal length (cm)','petal width (cm)']]
+y = iris[['target']].values
+
+model = LinearSVC()
+model.fit(X, y.ravel())
+
+# Create input array
+X_predict = targets[['petal length (cm)', 'petal width (cm)']]
+
+# Predict with the model
+predictions = model.predict(X_predict)
+print(predictions)
+
+# Visualize predictions and actual values
+plt.scatter(X_predict['petal length (cm)'], X_predict['petal width (cm)'],
+            c=predictions, cmap=plt.cm.coolwarm)
+plt.title("Predicted class values")
+plt.show()

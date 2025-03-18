eb2ff14e1e0fbea869d3b5203fec73e620aede5a
Arctaea <joycacao@gmail.com>
2025-03-18 14:47:06 +0800

    Database Design Database Views: view(create, join, drop cascade/restrict, access grant/revoke, redefine, materialized and refresh)

--- Track_SQL_Server_Developer/06_Course_Database_Design/39 view access.py
+++ Track_SQL_Server_Developer/06_Course_Database_Design/39 view access.py
@@ -1,22 +1,39 @@
--- Create a view with the top artists in 2017
+import pyodbc
+import pandas as pd
+
+conn = pyodbc.connect( "DRIVER={SQL Server};" "SERVER=Arc-PC;" "DATABASE=DatabaseDesign;" "Trusted_Connection=True;" )
+cursor = conn.cursor()
+
+cursor.execute("""
 DROP VIEW IF EXISTS long_reviews;
-GO
+""")
+conn.commit()
+
+cursor.execute("""
 CREATE VIEW long_reviews AS
 SELECT content.reviewid,
     content.content
    FROM content
   --WHERE (len(content.content) > 4000);
   WHERE DATALENGTH(content.content) > 4000;
-GO
+""")
+conn.commit()
 
+cursor.execute("""
 -- Revoke everyone's update and insert privileges
 REVOKE UPDATE, INSERT ON long_reviews FROM public;
-GO
+""")
+conn.commit()
 
+cursor.execute("""
 -- Grant the editor update and insert privileges 
 GRANT UPDATE, INSERT ON long_reviews TO public;
-GO
+""")
+conn.commit()
 
+
+select_query = """
+-- Get all non-systems views
 SELECT * FROM information_schema.views
 WHERE table_schema not in ('pg_catalog', 'information_schema');
 
@@ -34,3 +51,22 @@ JOIN
     sys.database_principals pr ON dp.grantee_principal_id = pr.principal_id
 WHERE 
     ob.name = 'long_reviews';
+"""
+
+cursor.execute(select_query)
+
+results = []
+while True:
+    rows = cursor.fetchall()
+    if not rows:
+        break
+    columns = [column[0] for column in cursor.description]
+    df = pd.DataFrame.from_records(rows, columns=columns)
+    results.append(df)
+    if not cursor.nextset():
+        break
+    
+for i, df in enumerate(results, start=1):
+    print(f'{i} SELECT STATEMENT\n, {df}\n')
+cursor.close()
+conn.close()

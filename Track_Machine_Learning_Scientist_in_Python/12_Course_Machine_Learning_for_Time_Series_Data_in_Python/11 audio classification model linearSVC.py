d5e303952dc077b81706a3bb8c335a694358f638
Arctaea <joycacao@gmail.com>
2025-03-19 22:03:45 +0800

    Machine Learning for Time Series Data Time Series as Inputs to a Model: audio(raw, rectify, envelope), feature (stats, derivative tempogram, spectrogram, specshow, spectral features)

--- Track_Machine_Learning_Scientist_in_Python/12_Course_Machine_Learning_for_Time_Series_Data_in_Python/11 audio classification model linearSVC.py
+++ Track_Machine_Learning_Scientist_in_Python/12_Course_Machine_Learning_for_Time_Series_Data_in_Python/11 audio classification model linearSVC.py
@@ -9,7 +9,7 @@ from sklearn.svm import LinearSVC
 
 # Initialize and fit the model
 model = LinearSVC()
-model.fit(X_train,y_train)
+model.fit(X_train,y_train.values.ravel())
 
 # Generate predictions and score them manually
 predictions = model.predict(X_test)
---
+++ Track_Machine_Learning_Scientist_in_Python/12_Course_Machine_Learning_for_Time_Series_Data_in_Python/debug/20 audio spectral features classifier copy 2.py
@@ -0,0 +1,135 @@
+import pandas as pd
+import numpy as np
+import os
+
+audio = np.array(pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\12_Course_Machine_Learning_for_Time_Series_Data_in_Python\datasets\audio_0.csv', header=None))
+audio = audio[:, 0]
+
+# Import the stft function
+from librosa.core import stft
+
+# Prepare the STFT
+HOP_LENGTH = 2**4
+# spec = stft(audio, hop_length=HOP_LENGTH, n_fft=2**7)
+spec = np.abs(stft(audio, hop_length=HOP_LENGTH, n_fft=2**7))
+
+import matplotlib.pyplot as plt
+from librosa.core import amplitude_to_db
+from librosa.display import specshow
+
+time = np.array(pd.read_csv(r'Track_Machine_Learning_Scientist_in_Python/12_Course_Machine_Learning_for_Time_Series_Data_in_Python/datasets/abnormal.csv', index_col=0).index)
+sfreq = 2205
+
+# Convert into decibels
+spec_db = amplitude_to_db(spec)
+
+# Compare the raw audio to the spectrogram of the audio
+fig, axs = plt.subplots(2, 1, figsize=(10, 10), sharex=True)
+axs[0].plot(time, audio)
+specshow(spec_db, sr=sfreq, x_axis='time', y_axis='hz', hop_length=HOP_LENGTH, ax=axs[1])
+plt.show()
+
+import librosa as lr
+
+# Calculate the spectral centroid and bandwidth for the spectrogram
+bandwidths = lr.feature.spectral_bandwidth(S=spec)[0]
+centroids = lr.feature.spectral_centroid(S=spec)[0]
+
+times_spec = np.array(pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\12_Course_Machine_Learning_for_Time_Series_Data_in_Python\datasets\times_spec.csv', header=None))
+times_spec = times_spec.flatten()
+
+# Convert spectrogram to decibels for visualization
+spec_db = amplitude_to_db(spec)
+
+# Display these features on top of the spectrogram
+fig, ax = plt.subplots(figsize=(10, 5))
+specshow(spec_db, x_axis='time', y_axis='hz', hop_length=HOP_LENGTH, ax=ax)
+ax.plot(times_spec, centroids)
+ax.fill_between(times_spec, centroids - bandwidths / 2, centroids + bandwidths / 2, alpha=.5)
+ax.set(ylim=[None, 6000])
+plt.show()
+
+
+
+
+
+
+
+
+
+
+HOP_LENGTH = 2**4
+N_FFT = 2**7
+
+spectrograms = []
+folder = 'D:\\STUDY\\python\\Track_Machine_Learning_Scientist_in_Python\\12_Course_Machine_Learning_for_Time_Series_Data_in_Python\\datasets\\specto\\'
+
+for file in os.listdir(folder):
+    filepath = os.path.join(folder, file)
+    spec = np.array(pd.read_csv(filepath))
+    # spec = np.array(pd.read_csv(filepath), dtype=np.float64)
+    spectrograms.append(spec)
+
+# spectrograms = spectrograms.astype(float)
+# spectrograms = list(np.float64(spectrograms))
+# [float(i) for i in spectrograms]
+# map(float, spectrograms)
+
+# spectrograms = [spec.astype(np.float64) for spec in spectrograms]
+# print(spectrograms[0].dtype)
+
+# for spec in spectrograms:
+#     print(spec.dtype)
+#     print(spec.shape)
+    # print(spec)
+    # break  # Just print the first one
+
+
+# Loop through each spectrogram
+bandwidths = []
+centroids = []
+means = []
+stds = []
+maxs = []
+
+for spec in spectrograms:
+    # Calculate the mean spectral bandwidth
+    this_mean_bandwidth = np.mean(lr.feature.spectral_bandwidth(S=spec))
+    # Calculate the mean spectral centroid
+    this_mean_centroid = np.mean(lr.feature.spectral_centroid(S=spec))
+    # Collect the values
+    bandwidths.append(this_mean_bandwidth)  
+    centroids.append(this_mean_centroid)
+    print(np.mean(spec))
+    means.append(np.mean(spec))
+    stds.append(np.std(spec))
+    maxs.append(np.max(spec))
+
+from sklearn.model_selection import cross_val_score
+from librosa.feature.rhythm import tempo
+print(means[0].shape)
+# audio_rectified = audio.apply(np.abs)
+# audio_rectified_smooth = audio_rectified.rolling(50).mean()
+# means = np.array(np.mean(audio_rectified_smooth, axis=0))
+# stds = np.array(np.std(audio_rectified_smooth, axis=0))
+# maxs = np.array(np.max(audio_rectified_smooth, axis=0))
+# tempos = []
+# for col, i_audio in audio.items():
+#     tempos.append(tempo(y=i_audio.values, sr=sfreq, hop_length=2**6, aggregate=None))
+
+# tempos = np.array(tempos)
+# tempos_mean = tempos.mean(axis=-1)
+# tempos_std = tempos.std(axis=-1)
+# tempos_max = tempos.max(axis=-1)
+
+print(means[0])
+print(stds[0])
+print(maxs[0])
+# print(means.shape)
+# # Create X and y arrays
+# X = np.column_stack([means, stds, maxs, tempo_mean, tempo_max, tempo_std, bandwidths, centroids])
+# y = labels.reshape(-1, 1)
+
+# # Fit the model and score on testing data
+# percent_score = cross_val_score(model, X, y, cv=5)
+# print(np.mean(percent_score))

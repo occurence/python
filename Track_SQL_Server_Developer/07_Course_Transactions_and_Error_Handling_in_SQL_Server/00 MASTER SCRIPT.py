828eb729670efeb909cac9e1c1a41015f3fdd5b5
Arctaea <joycacao@gmail.com>
2025-03-22 18:25:09 +0800

    SSMS database Starting with Error Handling: master script update

--- Track_SQL_Server_Developer/07_Course_Transactions_and_Error_Handling_in_SQL_Server/00 MASTER SCRIPT.py
+++ Track_SQL_Server_Developer/07_Course_Transactions_and_Error_Handling_in_SQL_Server/00 MASTER SCRIPT.py
@@ -3,6 +3,16 @@ import os
 
 scripts_to_run = [
     # "00 LIST FILES.py",
+    "01 schema database tehandling.py",
+    "02 try catch fail.py",
+    "03 try catch success.py",
+    "04 alter.py",
+    "05 try catch nested.py",
+    "06 error list.py",
+    "07 errors uncaught.py",
+    "08 errors compilation.py",
+    "09 errors function.py",
+    "10 errors function nested.py",
 ]
 
 SCRIPT_DIR = "D:\\STUDY\\python\\Track_SQL_Server_Developer\\07_Course_Transactions_and_Error_Handling_in_SQL_Server\\"

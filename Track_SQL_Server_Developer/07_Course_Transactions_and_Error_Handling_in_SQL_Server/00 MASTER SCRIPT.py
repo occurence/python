b40cb6a2d9fddfd081de8dd1b41a4e871fce9ccb
Arctaea <joycacao@gmail.com>
2025-03-26 17:16:12 +0800

    SSMS database Transactions: master script update

--- Track_SQL_Server_Developer/07_Course_Transactions_and_Error_Handling_in_SQL_Server/00 MASTER SCRIPT.py
+++ Track_SQL_Server_Developer/07_Course_Transactions_and_Error_Handling_in_SQL_Server/00 MASTER SCRIPT.py
@@ -15,6 +15,27 @@ scripts_to_run = [
     "10 errors function nested.py",
     "11 errors cast.py",
     "12 errors function nested.py",
+    "13.1 raiserror success.py",
+    # "13.2 raiserror fail.py",
+    "13.3 raiserror try catch success.py",
+    "13.4 raiserror try catch fail.py",
+    "14 throw noparam.py",
+    "15 throw message.py",
+    "16 throw param.py",
+    "17.1 throw concat success.py",
+    # "17.2 throw concat fail.py",
+    "18.1 throw formatmessage string success.py",
+    # "18.2 throw formatmessage string fail.py",
+    "19.1 throw formatmessage number success.py",
+    # "19.2 throw formatmessage number fail.py",
+    # "20 transaction commit.py", #
+    # "21 transaction rollback.py",
+    # "22 transaction begin tran.py", #
+    # "23 transaction trancount.py", #
+    # "24 transaction savepoint.py", #
+    # "25 transaction xact throw.py",
+    # "26.1 transaction xact doom XACT_STATE.py",
+    # "26.2 transaction xact doom no XACT_STATE.py",
 ]
 
 SCRIPT_DIR = "D:\\STUDY\\python\\Track_SQL_Server_Developer\\07_Course_Transactions_and_Error_Handling_in_SQL_Server\\"

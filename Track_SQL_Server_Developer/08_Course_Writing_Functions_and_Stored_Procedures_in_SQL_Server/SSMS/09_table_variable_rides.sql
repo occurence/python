69ffc77525dc700da2c13f7a1dced403290582ba
Arctaea <joycacao@gmail.com>
2025-03-28 19:55:19 +0800

    Writing Functions and Stored Procedures Temporal EDA, Variables and Data Manipulation: SSMS queries

---
+++ Track_SQL_Server_Developer/08_Course_Writing_Functions_and_Stored_Procedures_in_SQL_Server/SSMS/09_table_variable_rides.sql
@@ -0,0 +1,17 @@
+-- Declare @RideDates
+DECLARE @RideDates TABLE(
+    -- Define RideStart column
+	RideStart date, 
+    -- Define RideEnd column
+    RideEnd date)
+-- Populate @RideDates
+INSERT INTO @RideDates(RideStart, RideEnd)
+-- Select the unique date values of StartDate and EndDate
+SELECT DISTINCT
+    -- Cast StartDate as date
+	CAST(StartDate as date),
+    -- Cast EndDate as date
+	CAST(EndDate as date) 
+FROM CapitalBikeShare 
+SELECT * 
+FROM @RideDates

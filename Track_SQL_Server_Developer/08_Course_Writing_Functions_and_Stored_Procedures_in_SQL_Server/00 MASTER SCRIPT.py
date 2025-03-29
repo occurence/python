bad2956b0b10f3e67d4294ea15f69f1072f40ee0
Arctaea <joycacao@gmail.com>
2025-03-29 13:05:48 +0800

    SSMS database User Defined Functions: master script update

--- Track_SQL_Server_Developer/08_Course_Writing_Functions_and_Stored_Procedures_in_SQL_Server/00 MASTER SCRIPT.py
+++ Track_SQL_Server_Developer/08_Course_Writing_Functions_and_Stored_Procedures_in_SQL_Server/00 MASTER SCRIPT.py
@@ -13,6 +13,15 @@ scripts_to_run = [
     # "09_table_variable_rides.py",
     # "10_date_param.py",
     # "11_date_first_month.py",
+    "12 function noparam GetYesterday.py",
+    "13 function param SumRideHrsSingleDay.py",
+    "14 function params SumRideHrsDateRange.py",
+    "15 function params SumStationStats.py",
+    "16 function params CountTripAvgDuration.py",
+    # "17 function scalar select.py",
+    # "18 function scalar exec.py",
+    # "19 function table variable.py",
+    "20 function schemabinding create or alter.py",
 ]
 
 SCRIPT_DIR = "D:\\STUDY\\python\\Track_SQL_Server_Developer\\07_Course_Transactions_and_Error_Handling_in_SQL_Server\\"

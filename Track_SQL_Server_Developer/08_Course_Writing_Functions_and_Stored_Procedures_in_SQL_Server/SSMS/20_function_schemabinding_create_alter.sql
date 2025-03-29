b22e0ae65ec60b9d511233fc6900b8a2f381851c
Arctaea <joycacao@gmail.com>
2025-03-29 13:08:16 +0800

    Writing Functions and Stored Procedures User Defined Functions: create function(no param, param, params, create or alter with schemabinding) and usage function(scalar select, scalar exec, table variable)

--- Track_SQL_Server_Developer/08_Course_Writing_Functions_and_Stored_Procedures_in_SQL_Server/SSMS/20_function_schemabinding_create_alter.sql
+++ Track_SQL_Server_Developer/08_Course_Writing_Functions_and_Stored_Procedures_in_SQL_Server/SSMS/20_function_schemabinding_create_alter.sql
@@ -12,3 +12,11 @@ FROM dbo.CapitalBikeShare
 -- Cast EndDate as date and compare to @EndDate
 WHERE CAST(EndDate AS Date) = @EndDate
 GROUP BY StartStation;
+
+SELECT 
+    name, 
+    type_desc, 
+    OBJECT_DEFINITION(object_id) AS definition
+FROM sys.objects
+WHERE type IN ('FN', 'IF', 'TF')
+ORDER BY name;

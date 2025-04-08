7e9a3e9e4893edb25b9ba82ff6f0e38d2ef732ae
Arctaea <joycacao@gmail.com>
2025-04-08 19:13:24 +0800

    Hyperparameter Tuning Hyperparameters and Parameters: parameter model attributes(coefficient, tree feature and threshold), hyperparameters assessments(predictions, confusion matrix, accuracy score, learning rate) and automation for models(LogisticRegression, RandomForestClassifier, KNeighborsClassifier, and GradientBoostingClassifier)

---
+++ Track_Machine_Learning_Scientist_in_Python/15_Course_Hyperparameter_Tuning_in_Python/02 LogisticRegression parameter.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+
+cc = pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\15_Course_Hyperparameter_Tuning_in_Python\datasets\creditcard.csv', index_col=0)
+index = pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\15_Course_Hyperparameter_Tuning_in_Python\datasets\index.csv', index_col=0)
+cc_base = cc.drop(['SEX', 'EDUCATION', 'MARRIAGE', 'default payment next month'], axis=1).astype(int)
+cc_enc = cc[['SEX', 'EDUCATION', 'MARRIAGE']]
+ohe = OneHotEncoder(sparse_output=False, drop= 'first')
+enc = ohe.fit_transform(cc_enc).astype(int)
+cc_enc_df = pd.DataFrame(enc, columns=ohe.get_feature_names_out())
+
+X = pd.concat([cc_base, cc_enc_df], axis=1)
+
+X_train = X.iloc[index.index]
+# print(X_train)
+y_train = cc[['default payment next month']].iloc[index.index]
+y_train = y_train.values.ravel()
+# print(y_train)
+
+from sklearn.linear_model import LogisticRegression
+log_reg_clf = LogisticRegression()
+log_reg_clf.fit(X_train, y_train)
+
+# Create a list of original variable names from the training DataFrame
+original_variables = list(X_train.columns)
+
+# Extract the coefficients of the logistic regression estimator
+model_coefficients = log_reg_clf.coef_[0]
+
+# Create a dataframe of the variables and coefficients & print it out
+coefficient_df = pd.DataFrame({"Variable" : original_variables, "Coefficient": model_coefficients})
+print(coefficient_df)
+
+# Print out the top 3 positive variables
+top_three_df = coefficient_df.sort_values(by=['Coefficient'], axis=0, ascending=False)[0:3]
+print(top_three_df)

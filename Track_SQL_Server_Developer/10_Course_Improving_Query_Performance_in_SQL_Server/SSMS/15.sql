9b8d381dfaf89b3be7e9c07698af4ef0867ec8e9
Arctaea <joycacao@gmail.com>
2025-04-09 19:34:14 +0800

    Improving Query Performance Introduction, Review and the Order of Things: SSMS queries

---
+++ Track_SQL_Server_Developer/10_Course_Improving_Query_Performance_in_SQL_Server/SSMS/15.sql
@@ -0,0 +1,14 @@
+/*
+Returns the location of the epicenter of earthquakes with a 9+ magnitude
+*/
+
+-- Replace Magnatud with the correct column name
+SELECT n.CountryName AS Country
+	,e.NearestPop AS ClosestCity
+    ,e.Date
+    ,e.Magnitude
+FROM Nations AS n
+INNER JOIN Earthquakes AS e
+	ON n.Code2 = e.Country
+WHERE e.Magnitude >= 9
+ORDER BY e.Magnitude DESC;

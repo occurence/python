4e1139756bd2745127b501798fc2d6e034cd14a9
Arctaea <joycacao@gmail.com>
2025-04-22 12:57:42 +0800

    Natural Language Processing with spaCy spaCy Linguistic Annotations and Word Vectors: graph entity and dependency

--- Track_Machine_Learning_Scientist_in_Python/18_Course_Natural_Language_Processing_with_spaCy/02 tokenization.py
+++ Track_Machine_Learning_Scientist_in_Python/18_Course_Natural_Language_Processing_with_spaCy/02 tokenization.py
@@ -1,5 +1,5 @@
 import spacy
-
+from spacy import displacy
 nlp = spacy.load('en_core_web_sm')
 text = 'I have bought several of the Vitality canned dog food products and have found them all to be of good quality. The product looks more like a stew than a processed meat and it smells better. My Labrador is finicky and she appreciates this product better than  most.'
 
@@ -9,3 +9,6 @@ document = nlp(text)
 # Store and review the token text values of tokens for the Doc container
 first_text_tokens = [token.text for token in document]
 print("First text tokens:\n", first_text_tokens, "\n")
+
+displacy.serve(document, style='ent')
+displacy.serve(document, style='dep')

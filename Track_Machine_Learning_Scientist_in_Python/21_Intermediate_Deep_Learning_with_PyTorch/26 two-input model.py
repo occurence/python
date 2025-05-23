8f86a325186949a6ac10df5210bedfd1b7590d06
Arctaea <joycacao@gmail.com>
2025-05-23 23:00:00 +0800

    Intermediate Deep Learning with PyTorch Multi-Input & Multi-Output Architectures: two-input model update dataset, dataloader, and samples files

--- Track_Machine_Learning_Scientist_in_Python/21_Intermediate_Deep_Learning_with_PyTorch/26 two-input model.py
+++ Track_Machine_Learning_Scientist_in_Python/21_Intermediate_Deep_Learning_with_PyTorch/26 two-input model.py
@@ -1,5 +1,18 @@
 import torch
 import torch.nn as nn
+import numpy as np
+from PIL import Image
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+
+samples = [('D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\21_Intermediate_Deep_Learning_with_PyTorch\datasets\omniglot_train\Gujarati\character42\0459_16.png',
+  np.array([1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+         0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.], dtype='float32'),
+  0),
+ ('D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\21_Intermediate_Deep_Learning_with_PyTorch\datasets\omniglot_train\Gujarati\character42\0459_02.png',
+  np.array([1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+         0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.], dtype='float32'),
+  0)]
 
 class Net(nn.Module):
     def __init__(self):
@@ -27,3 +40,33 @@ class Net(nn.Module):
         # Concatenate x_image and x_alphabet
         x = torch.cat((x_image, x_alphabet), dim=1)
         return self.classifier(x)
+
+class OmniglotDataset(Dataset):
+    def __init__(self, transform, samples):
+		# Assign transform and samples to class attributes
+        self.transform = transform
+        self.samples = samples
+                    
+    def __len__(self):
+		# Return number of samples
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+      	# Unpack the sample at index idx
+        img_path, alphabet, label = self.samples[idx]
+        img = Image.open(img_path).convert('L')
+        # Transform the image 
+        img_transformed = self.transform(img)
+        return img_transformed, alphabet, label
+    
+dataset_train = OmniglotDataset(
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((64, 64)),
+    ]),
+    samples=samples,
+)
+
+dataloader_train = DataLoader(
+    dataset_train, shuffle=True, batch_size=3,
+)

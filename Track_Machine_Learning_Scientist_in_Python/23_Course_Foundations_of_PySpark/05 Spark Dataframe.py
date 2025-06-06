c64dc5e08fc6d6fd96802949fd06afde32d8a519
Arctaea <joycacao@gmail.com>
2025-06-06 21:00:00 +0800

    Intermediate Deep Learning with PyTorch Getting to know PySpark: dataframe, data, and filepath

---
+++ Track_Machine_Learning_Scientist_in_Python/23_Course_Foundations_of_PySpark/05 Spark Dataframe.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+print(spark)
+
+# Don't change this query
+query = "SELECT origin, dest, COUNT(*) as N FROM flights GROUP BY origin, dest"
+
+# Run the query
+flight_counts = spark.sql(query)
+
+# Convert the results to a pandas DataFrame
+pd_counts = flight_counts.toPandas()
+
+# Print the head of pd_counts
+print(pd_counts.head())

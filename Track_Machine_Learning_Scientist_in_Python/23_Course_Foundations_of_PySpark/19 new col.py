247d39b54ce3d8fe279b0f76436593b86a1deb99
Arctaea <joycacao@gmail.com>
2025-06-10 20:07:12 +0800

    Foundations of PySpark Getting started with machine learning pipelines: cast and new column

---
+++ Track_Machine_Learning_Scientist_in_Python/23_Course_Foundations_of_PySpark/19 new col.py
@@ -0,0 +1,28 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, when
+
+spark = SparkSession.builder \
+    .appName("FlightsExample") \
+    .getOrCreate()
+
+flights_df = spark.read.csv(r"D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\23_Course_Foundations_of_PySpark\datasets\flights_small.csv", header=True, inferSchema=True)
+flights_df = flights_df.withColumn("air_time",
+    when(col("air_time") == "NA", None).otherwise(col("air_time")).cast("float")
+).withColumn("dep_delay",
+    when(col("dep_delay") == "NA", None).otherwise(col("dep_delay")).cast("float")
+)
+flights_df.createOrReplaceTempView("flights")
+flights = spark.table("flights")
+
+flights = flights.withColumnRenamed("year", "plane_year")
+
+# Cast the columns to integers
+model_data = flights.withColumn("arr_delay", flights.arr_delay.cast("integer"))
+model_data = flights.withColumn("air_time", flights.air_time.cast("integer"))
+model_data = flights.withColumn("month", flights.month.cast("integer"))
+model_data = flights.withColumn("plane_year", flights.plane_year.cast("integer"))
+
+# Create the column plane_age
+model_data = model_data.withColumn("plane_age", flights.year - flights.plane_year)
+
+model_data.show()

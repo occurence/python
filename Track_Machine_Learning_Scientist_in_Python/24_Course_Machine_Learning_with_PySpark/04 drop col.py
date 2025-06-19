48c58fe05be203295c8fd626189bb3094ce284eb
Arctaea <joycacao@gmail.com>
2025-06-19 17:00:00 +0800

    Machine Learning with PySpark Classification: drop missing rows and columns

---
+++ Track_Machine_Learning_Scientist_in_Python/24_Course_Machine_Learning_with_PySpark/04 drop col.py
@@ -0,0 +1,28 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+                    .master('local[*]') \
+                    .appName('test') \
+                    .getOrCreate()
+
+# Read data from CSV file
+flights = spark.read.csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\24_Course_Machine_Learning_with_PySpark\datasets\flights.csv',
+                         sep=',',
+                         header=True,
+                         inferSchema=True,
+                         nullValue='NA')
+
+# Remove the 'flight' column
+flights_drop_column = flights.drop('flight')
+
+# Number of records with missing 'delay' values
+flights_drop_column.filter('delay IS NULL').count()
+
+# Remove records with missing 'delay' values
+flights_valid_delay = flights_drop_column.filter('delay IS NOT NULL')
+
+# Remove records with missing values in any column and get the number of remaining rows
+flights_none_missing = flights_valid_delay.dropna()
+print(flights_none_missing.count())
+
+spark.stop()

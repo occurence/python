aaf4fd40e48ea30c1a762e22f3389cf7043b6c01
Arctaea <joycacao@gmail.com>
2025-06-20 19:58:33 +0800

    Machine Learning with PySpark Classification: manipulate when filter and cast, column category transform to numeric index

---
+++ Track_Machine_Learning_Scientist_in_Python/24_Course_Machine_Learning_with_PySpark/05 manipulation.py
@@ -0,0 +1,41 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+                    .master('local[*]') \
+                    .appName('test') \
+                    .getOrCreate()
+
+# Read data from CSV file
+flights = spark.read.csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\24_Course_Machine_Learning_with_PySpark\datasets\flights.csv',
+                         sep=',',
+                         header=True,
+                         inferSchema=True,
+                         nullValue='NA')
+
+# Remove the 'flight' column
+flights_drop_column = flights.drop('flight')
+
+# Number of records with missing 'delay' values
+flights_drop_column.filter('delay IS NULL').count()
+
+# Remove records with missing 'delay' values
+flights_valid_delay = flights_drop_column.filter('delay IS NOT NULL')
+
+# Remove records with missing values in any column and get the number of remaining rows
+flights_none_missing = flights_valid_delay.dropna()
+print(flights_none_missing.count())
+
+# Import the required function
+from pyspark.sql.functions import round, when
+
+# Convert 'mile' to 'km' and drop 'mile' column (1 mile is equivalent to 1.60934 km)
+flights_km = flights.withColumn('km', round(flights.mile * 1.60934, 0)) \
+                    .drop('mile')
+
+# Create 'label' column indicating whether flight delayed (1) or not (0)
+flights_km = flights_km.withColumn('label', when(flights_km.delay >= 15,1).otherwise(0).cast('integer'))
+
+# Check first five records
+flights_km.show(5)
+
+spark.stop()
---
+++ Track_Machine_Learning_Scientist_in_Python/24_Course_Machine_Learning_with_PySpark/06 categorical.py
@@ -0,0 +1,56 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+                    .master('local[*]') \
+                    .appName('test') \
+                    .getOrCreate()
+
+# Read data from CSV file
+flights = spark.read.csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\24_Course_Machine_Learning_with_PySpark\datasets\flights.csv',
+                         sep=',',
+                         header=True,
+                         inferSchema=True,
+                         nullValue='NA')
+
+# Remove the 'flight' column
+flights_drop_column = flights.drop('flight')
+
+# Number of records with missing 'delay' values
+flights_drop_column.filter('delay IS NULL').count()
+
+# Remove records with missing 'delay' values
+flights_valid_delay = flights_drop_column.filter('delay IS NOT NULL')
+
+# Remove records with missing values in any column and get the number of remaining rows
+flights_none_missing = flights_valid_delay.dropna()
+print(flights_none_missing.count())
+
+# Import the required function
+from pyspark.sql.functions import round, when
+
+# Convert 'mile' to 'km' and drop 'mile' column (1 mile is equivalent to 1.60934 km)
+flights_km = flights.withColumn('km', round(flights.mile * 1.60934, 0)) \
+                    .drop('mile')
+
+# Create 'label' column indicating whether flight delayed (1) or not (0)
+flights_km = flights_km.withColumn('label', when(flights_km.delay >= 15,1).otherwise(0).cast('integer'))
+
+# Check first five records
+flights_km.show(5)
+
+from pyspark.ml.feature import StringIndexer
+
+# Create an indexer
+indexer = StringIndexer(inputCol='carrier', outputCol='carrier_idx')
+
+# Indexer identifies categories in the data
+indexer_model = indexer.fit(flights)
+
+# Indexer creates a new column with numeric index values
+flights_indexed = indexer_model.transform(flights)
+
+# Repeat the process for the other categorical feature
+flights_indexed = StringIndexer(inputCol='org', outputCol='org_idx').fit(flights_indexed).transform(flights_indexed)
+flights_indexed.show(5)
+
+spark.stop()

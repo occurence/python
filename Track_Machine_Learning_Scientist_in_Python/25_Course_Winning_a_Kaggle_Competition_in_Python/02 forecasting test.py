4612c8357c88b412c826c55ca7e2147c9418f406
Arctaea <joycacao@gmail.com>
2025-07-07 16:11:39 +0800

    Winning a Kaggle Competition in Python Kaggle competitions process: extracting train and test files to dataframe

---
+++ Track_Machine_Learning_Scientist_in_Python/25_Course_Winning_a_Kaggle_Competition_in_Python/02 forecasting test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Read train data
+train = pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\25_Course_Winning_a_Kaggle_Competition_in_Python\datasets\demand_forecasting_train_1_month.csv')
+# Read the test data
+test = pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\25_Course_Winning_a_Kaggle_Competition_in_Python\datasets\demand_forecasting_test.csv')
+# Print train and test columns
+print('Train columns:', train.columns.tolist())
+print('Test columns:', test.columns.tolist())
+
+# Read the sample submission file
+sample_submission = pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\25_Course_Winning_a_Kaggle_Competition_in_Python\datasets\sample.csv')
+
+# Look at the head() of the sample submission
+print(sample_submission.head())

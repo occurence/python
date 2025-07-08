270004d5eb7e7dc9e998008e71d2009621f83116
Arctaea <joycacao@gmail.com>
2025-07-08 16:15:32 +0800

    Winning a Kaggle Competition in Python Kaggle competitions process: identify problem type, get data files, write test predictions as submission

--- Track_Machine_Learning_Scientist_in_Python/25_Course_Winning_a_Kaggle_Competition_in_Python/02 forecasting test.py
+++ Track_Machine_Learning_Scientist_in_Python/25_Course_Winning_a_Kaggle_Competition_in_Python/02 forecasting test.py
@@ -9,7 +9,7 @@ print('Train columns:', train.columns.tolist())
 print('Test columns:', test.columns.tolist())
 
 # Read the sample submission file
-sample_submission = pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\25_Course_Winning_a_Kaggle_Competition_in_Python\datasets\sample.csv')
+sample_submission = pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\25_Course_Winning_a_Kaggle_Competition_in_Python\datasets\demand_submission.csv')
 
 # Look at the head() of the sample submission
 print(sample_submission.head())

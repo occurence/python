23b84d1200ffebfdcd8e16f1de1fab8d533b99c3
Arctaea <joycacao@gmail.com>
2025-07-10 19:42:05 +0800

    Winning a Kaggle Competition in Python Dive into the Competition: competition metric functions(MSE and LogLoss)

---
+++ Track_Machine_Learning_Scientist_in_Python/25_Course_Winning_a_Kaggle_Competition_in_Python/08 metric function LogLoss.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+# Import log_loss from sklearn
+from sklearn.metrics import log_loss
+
+# Define your own LogLoss function
+def own_logloss(y_true, prob_pred):
+  	# Find loss for each observation
+    terms = y_true * np.log(prob_pred) + (1 - y_true) * np.log(1 - prob_pred)
+    # Find mean over all observations
+    err = np.mean(terms) 
+    return -err
+
+y_classification_true = pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\25_Course_Winning_a_Kaggle_Competition_in_Python\datasets\classification_true.csv', header=None).to_numpy()
+y_classification_pred = pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\25_Course_Winning_a_Kaggle_Competition_in_Python\datasets\regression_predict.csv', header=None).to_numpy()
+
+print('Sklearn LogLoss: {:.5f}'.format(log_loss(y_classification_true, y_classification_pred)))
+print('Your LogLoss: {:.5f}'.format(own_logloss(y_classification_true, y_classification_pred)))

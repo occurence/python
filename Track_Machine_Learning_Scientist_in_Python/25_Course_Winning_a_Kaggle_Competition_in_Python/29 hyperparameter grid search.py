fb3f5461e10ce2d4ce6a4a51b87cb0cf5f5b2017
Arctaea <joycacao@gmail.com>
2025-07-19 22:12:28 +0800

    Winning a Kaggle Competition in Python Modeling: hyperparameter tuning(grid search and 2D)

---
+++ Track_Machine_Learning_Scientist_in_Python/25_Course_Winning_a_Kaggle_Competition_in_Python/29 hyperparameter grid search.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import KFold
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error
+
+def haversine_distance(train):
+    
+    data = [train]
+    lat1, long1, lat2, long2 = 'pickup_latitude', 'pickup_longitude', 'dropoff_latitude', 'dropoff_longitude'
+    
+    for i in data:
+        R = 6371  #radius of earth in kilometers
+        #R = 3959 #radius of earth in miles
+        phi1 = np.radians(i[lat1])
+        phi2 = np.radians(i[lat2])
+    
+        delta_phi = np.radians(i[lat2]-i[lat1])
+        delta_lambda = np.radians(i[long2]-i[long1])
+    
+        #a = sin²((φB - φA)/2) + cos φA . cos φB . sin²((λB - λA)/2)
+        a = np.sin(delta_phi / 2.0) ** 2 + np.cos(phi1) * np.cos(phi2) * np.sin(delta_lambda / 2.0) ** 2
+    
+        #c = 2 * atan2( √a, √(1−a) )
+        c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
+    
+        #d = R*c
+        d = (R * c) #in kilometers
+        
+    return d
+
+train = pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\25_Course_Winning_a_Kaggle_Competition_in_Python\datasets\taxi_train_chapter_4.csv')
+train = train.iloc[:1000]
+train['distance_km'] = haversine_distance(train)
+train['pickup_datetime'] = pd.to_datetime(train.pickup_datetime)
+train['hour'] = train.pickup_datetime.dt.hour
+features = ['pickup_longitude','pickup_latitude','dropoff_longitude','dropoff_latitude','passenger_count','distance_km','hour']
+
+def get_cv_score(train, params):
+    # Create KFold object
+    kf = KFold(n_splits=3, shuffle=True, random_state=123)
+
+    rmse_scores = []
+    
+    # Loop through each split
+    for train_index, test_index in kf.split(train):
+        cv_train, cv_test = train.iloc[train_index], train.iloc[test_index]
+    
+        # Train a Gradient Boosting model
+        gb = GradientBoostingRegressor(random_state=123, **params).fit(cv_train[features], cv_train.fare_amount)
+    
+        # Make predictions on the test data
+        pred = gb.predict(cv_test[features])
+    
+        fold_score = np.sqrt(mean_squared_error(cv_test['fare_amount'], pred))
+        rmse_scores.append(fold_score)
+    
+    return np.round(np.mean(rmse_scores) + np.std(rmse_scores), 5)
+
+# Possible max depth values
+max_depth_grid = [3,6,9,12,15]
+results = {}
+
+# For each value in the grid
+for max_depth_candidate in max_depth_grid:
+    # Specify parameters for the model
+    params = {'max_depth': max_depth_candidate}
+
+    # Calculate validation score for a particular hyperparameter
+    validation_score = get_cv_score(train, params)
+
+    # Save the results for each max depth value
+    results[max_depth_candidate] = validation_score   
+print(results)

cb92f602a2149601a295153516475f02a002c588
Arctaea <joycacao@gmail.com>
2025-07-20 15:08:04 +0800

    Winning a Kaggle Competition in Python Modeling: model blending(GradientBoostingRegressor and RandomForestRegressor mean), model stacking I(part1 train, part2 predict, test predict, part2 predict with new feat, test predict with 2nd model) and model stacking II(part1 train, part2 predict, test predict)

---
+++ Track_Machine_Learning_Scientist_in_Python/25_Course_Winning_a_Kaggle_Competition_in_Python/31 model blend taxi.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import KFold
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error
+
+def haversine_distance(train):
+    
+    data = [train]
+    lat1, long1, lat2, long2 = 'pickup_latitude', 'pickup_longitude', 'dropoff_latitude', 'dropoff_longitude'
+    
+    for i in data:
+        R = 6371  #radius of earth in kilometers
+        #R = 3959 #radius of earth in miles
+        phi1 = np.radians(i[lat1])
+        phi2 = np.radians(i[lat2])
+    
+        delta_phi = np.radians(i[lat2]-i[lat1])
+        delta_lambda = np.radians(i[long2]-i[long1])
+    
+        #a = sin²((φB - φA)/2) + cos φA . cos φB . sin²((λB - λA)/2)
+        a = np.sin(delta_phi / 2.0) ** 2 + np.cos(phi1) * np.cos(phi2) * np.sin(delta_lambda / 2.0) ** 2
+    
+        #c = 2 * atan2( √a, √(1−a) )
+        c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
+    
+        #d = R*c
+        d = (R * c) #in kilometers
+        
+    return d
+
+features = ['pickup_longitude','pickup_latitude','dropoff_longitude','dropoff_latitude','passenger_count','distance_km','hour']
+train = pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\25_Course_Winning_a_Kaggle_Competition_in_Python\datasets\taxi_train_chapter_4.csv')
+train = train.iloc[:1000]
+train['distance_km'] = haversine_distance(train)
+train['pickup_datetime'] = pd.to_datetime(train.pickup_datetime)
+train['hour'] = train.pickup_datetime.dt.hour
+test = pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\25_Course_Winning_a_Kaggle_Competition_in_Python\datasets\taxi_test_chapter_4.csv')
+test = test.iloc[:1000]
+test['distance_km'] = haversine_distance(test)
+test['pickup_datetime'] = pd.to_datetime(test.pickup_datetime)
+test['hour'] = test.pickup_datetime.dt.hour
+
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+
+# Train a Gradient Boosting model
+gb = GradientBoostingRegressor().fit(train[features], train.fare_amount)
+
+# Train a Random Forest model
+rf = RandomForestRegressor().fit(train[features], train.fare_amount)
+
+# Make predictions on the test data
+test['gb_pred'] = gb.predict(test[features])
+test['rf_pred'] = rf.predict(test[features])
+
+# Find mean of model predictions
+test['blend'] = (test['gb_pred'] + test['rf_pred']) / 2
+print(test[['gb_pred', 'rf_pred', 'blend']].head(3))
---
+++ Track_Machine_Learning_Scientist_in_Python/25_Course_Winning_a_Kaggle_Competition_in_Python/32 model stack I.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+
+def haversine_distance(train):
+    
+    data = [train]
+    lat1, long1, lat2, long2 = 'pickup_latitude', 'pickup_longitude', 'dropoff_latitude', 'dropoff_longitude'
+    
+    for i in data:
+        R = 6371  #radius of earth in kilometers
+        #R = 3959 #radius of earth in miles
+        phi1 = np.radians(i[lat1])
+        phi2 = np.radians(i[lat2])
+    
+        delta_phi = np.radians(i[lat2]-i[lat1])
+        delta_lambda = np.radians(i[long2]-i[long1])
+    
+        #a = sin²((φB - φA)/2) + cos φA . cos φB . sin²((λB - λA)/2)
+        a = np.sin(delta_phi / 2.0) ** 2 + np.cos(phi1) * np.cos(phi2) * np.sin(delta_lambda / 2.0) ** 2
+    
+        #c = 2 * atan2( √a, √(1−a) )
+        c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
+    
+        #d = R*c
+        d = (R * c) #in kilometers
+        
+    return d
+
+features = ['pickup_longitude','pickup_latitude','dropoff_longitude','dropoff_latitude','passenger_count','distance_km','hour']
+train = pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\25_Course_Winning_a_Kaggle_Competition_in_Python\datasets\taxi_train_chapter_4.csv')
+train = train.iloc[:1000]
+train['distance_km'] = haversine_distance(train)
+train['pickup_datetime'] = pd.to_datetime(train.pickup_datetime)
+train['hour'] = train.pickup_datetime.dt.hour
+test = pd.read_csv(r'D:\STUDY\python\Track_Machine_Learning_Scientist_in_Python\25_Course_Winning_a_Kaggle_Competition_in_Python\datasets\taxi_test_chapter_4.csv')
+test = test.iloc[:1000]
+test['distance_km'] = haversine_distance(test)
+test['pickup_datetime'] = pd.to_datetime(test.pickup_datetime)
+test['hour'] = test.pickup_datetime.dt.hour
+
+# Split train data into two parts
+part_1, part_2 = train_test_split(train, test_size=0.5, random_state=123)
+
+# Train a Gradient Boosting model on Part 1
+gb = GradientBoostingRegressor().fit(part_1[features], part_1.fare_amount)
+
+# Train a Random Forest model on Part 1
+rf = RandomForestRegressor().fit(part_1[features], part_1.fare_amount)
+
+# Make predictions on the Part 2 data
+part_2['gb_pred'] = gb.predict(part_2[features])
+part_2['rf_pred'] = rf.predict(part_2[features])
+
+# Make predictions on the test data
+test['gb_pred'] = gb.predict(test[features])
+test['rf_pred'] = rf.predict(test[features])

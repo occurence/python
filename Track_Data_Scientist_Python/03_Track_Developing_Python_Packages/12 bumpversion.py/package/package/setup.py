948a39047ad474411db1e2d42c6c34bbc1b21719
Arctaea <joycacao@gmail.com>
2025-08-08 19:50:14 +0800

    Bump version: 0.1.0 → 0.2.0

--- Track_Data_Scientist_Python/03_Track_Developing_Python_Packages/12 bumpversion.py/package/package/setup.py
+++ Track_Data_Scientist_Python/03_Track_Developing_Python_Packages/12 bumpversion.py/package/package/setup.py
@@ -6,7 +6,7 @@ setup(
     description="A package for converting imperial lengths and weights.",
     name="impyrial",
     packages=find_packages(include=["impyrial", "impyrial.*"]),
-    version="0.1.0",
+    version="0.2.0",
     install_requires=['numpy>=1.10', 'pandas'],
     python_requires="==3.9.*",
 )

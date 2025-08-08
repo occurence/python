e6ab469b349015e4534631361eea01ff49652f90
Arctaea <joycacao@gmail.com>
2025-08-08 19:51:26 +0800

    Developing Python Packages Rapid Package Development: bumpversion config to change multiple files

--- Track_Data_Scientist_Python/03_Track_Developing_Python_Packages/12 bumpversion.py/package/package/impyrial/__init__.py
+++ Track_Data_Scientist_Python/03_Track_Developing_Python_Packages/12 bumpversion.py/package/package/impyrial/__init__.py
@@ -5,7 +5,7 @@ A package for converting between imperial measurments of length and weight.
 """
 
 __author__ = """James Fulton"""
-__version__ = "0.2.0"
+__version__ = "0.1.0"
 
 from . import length  # noqa : F401
 from . import weight  # noqa : F401

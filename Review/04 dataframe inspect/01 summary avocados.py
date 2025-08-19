9ab1d077a1dedaca84d436c453bf767a40c6d6b2
Arctaea <joycacao@gmail.com>
2025-08-19 20:12:26 +0800

    Review: Associate Data Scientist in Python Data Manipulation with pandas: data visualization alternative(manual condition, dynamic unique, apply groupby list)

--- Review/04 dataframe inspect/01 summary avocados.py
+++ Review/04 dataframe inspect/01 summary avocados.py
@@ -23,3 +23,36 @@ avocados[avocados["type"] == "conventional"]["avg_price"].hist(alpha=0.5, bins=2
 avocados[avocados["type"] == "organic"]["avg_price"].hist(alpha=0.5, bins=20)
 plt.legend(["conventional", "organic"])
 plt.show()
+
+avocados[avocados["type"] == "conventional"]["avg_price"].plot(
+    kind="hist",
+    bins=20,
+    alpha=0.5
+)
+avocados[avocados["type"] == "organic"]["avg_price"].plot(
+    kind="hist",
+    bins=20,
+    alpha=0.5
+)
+plt.legend(["conventional", "organic"])
+plt.show()
+
+
+# nb_avocados_type = avocados.groupby('type')['avg_price'].sum()
+# nb_avocados_type.plot(kind='hist')
+# plt.legend(["conventional", "organic"])
+# plt.show()
+
+for t in avocados["type"].unique():
+    avocados[avocados["type"] == t]["avg_price"].plot(kind="hist", bins=20, alpha=0.5, label=t)
+
+plt.legend()
+plt.show()
+
+prices_by_type = avocados.groupby("type")["avg_price"].apply(list)
+
+for t, values in prices_by_type.items():
+    plt.hist(values, bins=20, alpha=0.5, label=t)
+
+plt.legend()
+plt.show()

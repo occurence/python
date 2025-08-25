98d2982b9347ea29a59abe7441530227e2d9e509
Arctaea <joycacao@gmail.com>
2025-08-25 20:41:26 +0800

    Review: Associate Data Scientist in Python Joining Data with Pandas: merge_asof(forward/nearest, diff, list comprehension, bar plot)

---
+++ Review/05 join pandas/25 merge_asof create dataset.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+gdp = pd.read_csv(r'D:\STUDY\python\Review\05 join pandas\datasets\gdp_recession.csv', parse_dates=['date'])
+recession = pd.read_csv(r'D:\STUDY\python\Review\05 join pandas\datasets\recession.csv', parse_dates=['date'])
+
+# Merge gdp and recession on date using merge_asof()
+gdp_recession = pd.merge_asof(gdp, recession, on='date')
+
+# Create a list based on the row value of gdp_recession['econ_status']
+is_recession = ['r' if s=='recession' else 'g' for s in gdp_recession['econ_status']]
+
+# Plot a bar chart of gdp_recession
+gdp_recession.plot(kind='bar', y='gdp', x='date', color=is_recession, rot=90)
+plt.show()

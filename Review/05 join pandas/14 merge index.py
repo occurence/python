716481fa342ec73c7026da09977895224ca822ae
Arctaea <joycacao@gmail.com>
2025-09-15 18:39:40 +0800

    Review: Associate Data Scientist in Python Joining Data with Pandas: verify(on and index) and review suffixes

--- Review/05 join pandas/14 merge index.py
+++ Review/05 join pandas/14 merge index.py
@@ -6,7 +6,8 @@ ratings = pd.read_pickle(r'D:\STUDY\python\Review\05 join pandas\datasets\rating
 ratings.set_index(ratings.columns[0], inplace=True)
 
 # Merge to the movies table the ratings table on the index
-movies_ratings = movies.merge(ratings, how='left', left_on='id', right_index=True)
+# movies_ratings = movies.merge(ratings, how='left', left_on='id', right_index=True)
+movies_ratings = movies.merge(ratings, left_on='id', right_index=True)
 # Simple merge
 # movies_ratings = movies.merge(ratings, on='id', how='left')
 

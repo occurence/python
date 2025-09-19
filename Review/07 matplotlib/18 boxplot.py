ce5a0f29a2e3a8cf9124a6f0f77ae3726cd2887d
Arctaea <joycacao@gmail.com>
2025-09-19 23:35:35 +0800

    Review: Associate Data Scientist in Python Introduction with Data Visualization with Matplotlib: quantitative comparison: histogram(default and step), error bars for bar, line and box plots

---
+++ Review/07 matplotlib/18 boxplot.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+mens_rowing = pd.read_csv(r'D:\STUDY\python\Review\07 matplotlib\datasets\mens_rowing.csv', index_col=0)
+mens_gymnastics = pd.read_csv(r'D:\STUDY\python\Review\07 matplotlib\datasets\mens_gymnastics.csv', index_col=0)
+
+fig, ax = plt.subplots()
+
+# Add a boxplot for the "Height" column in the DataFrames
+ax.boxplot([mens_rowing['Height'], mens_gymnastics['Height']])
+
+# Add x-axis tick labels:
+ax.set_xticklabels(['Rowing', 'Gymnastics'])
+
+# Add a y-axis label
+ax.set_ylabel('Height (cm)')
+
+plt.show()

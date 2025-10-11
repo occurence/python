0fb5570feb1ee7b070a8f80577259db94051a6ce
Arctaea <joycacao@gmail.com>
2025-10-11 21:57:21 +0800

    Review: Associate Data Scientist in Python Introduction to Importing Data in Python: Introduction and flat files: flatfile and custom

---
+++ Review/13 import data/04 import flatfile.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+# Import packages
+import numpy as np
+
+# Assign filename to variable: file
+file = r'D:\STUDY\python\Review\13 import data\datasets\digits.csv'
+
+# Load file as array: digits
+digits = np.loadtxt(file, delimiter=',')
+
+# Print datatype of digits
+print(type(digits))
+
+# Select and reshape a row
+im = digits[21, 1:]
+im_sq = np.reshape(im, (28, 28))
+
+# Plot reshaped data (matplotlib.pyplot already loaded as plt)
+plt.imshow(im_sq, cmap='Greys', interpolation='nearest')
+plt.show()

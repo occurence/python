211559b12d7731bfe25fc9cde7f21431d3c786a1
Arctaea <joycacao@gmail.com>
2025-10-16 20:14:37 +0800

    Review:  Associate Data Scientist in Python Introduction to Importing Data in Python: Importing data from other file types: import sas7bdat and stata

---
+++ Review/13 import data/15 stata.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Import pandas
+import pandas as pd
+
+# Load Stata file into a pandas DataFrame: df
+df = pd.read_stata(r'D:\STUDY\python\Review\13 import data\datasets\disarea.dta')
+
+# Print the head of the DataFrame df
+print(df.head())
+
+# Plot histogram of one column of the DataFrame
+pd.DataFrame.hist(df[['disa10']])
+plt.xlabel('Extent of disease')
+plt.ylabel('Number of countries')
+plt.show()

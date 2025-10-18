30a299289e6796efd8989dfbcac1a79f60040f23
Arctaea <joycacao@gmail.com>
2025-10-18 22:37:51 +0800

    Review:  Associate Data Scientist in Python Introduction to Importing Data in Python: Importing data from other file types: import matlab files

---
+++ Review/13 import data/18 mat.py
@@ -0,0 +1,8 @@
+# Import package
+import scipy.io
+
+# Load MATLAB file: mat
+mat = scipy.io.loadmat(r'D:\STUDY\python\Review\13 import data\datasets\ja_data2.mat')
+
+# Print the datatype type of mat
+print(type(mat))

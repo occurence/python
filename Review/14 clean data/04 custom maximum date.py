3def216270388f17c5fb07b8254e7e911b037702
Arctaea <joycacao@gmail.com>
2025-10-26 20:52:42 +0800

    Review: Associate Data Scientist in Python Cleaning Data in Python: Common data problems: custom maximum date and finding duplicates

---
+++ Review/14 clean data/04 custom maximum date.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import datetime as dt
+
+ride_sharing = pd.read_csv(r'D:\STUDY\python\Review\14 clean data\datasets\ride_sharing_date.csv')
+
+# Convert ride_date to date
+ride_sharing['ride_dt'] = pd.to_datetime(ride_sharing['ride_date']).dt.date
+
+# Save today's date
+today = dt.date.today()
+
+# Set all in the future to today's date
+ride_sharing.loc[ride_sharing['ride_dt'] > today, 'ride_dt'] = today
+
+# Print maximum of ride_dt column
+print(ride_sharing['ride_dt'].max())

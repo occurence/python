5ecffe8ca04f08fd29ebab4438e459c949939832
Arctaea <joycacao@gmail.com>
2025-11-01 21:43:36 +0800

    Review: Associate Data Scientist in Python Cleaning Data in Python: Advanced data problems: uniformity with numbers and dates

---
+++ Review/14 clean data/12 clean uniformity date.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+banking = pd.read_csv(r'D:\STUDY\python\Review\14 clean data\datasets\banking.csv')
+
+# Print the header of account_opend
+print(banking['account_opened'].head())
+
+# Convert account_opened to datetime
+banking['account_opened'] = pd.to_datetime(banking['account_opened'],
+                                           # Return missing value for error
+                                           errors = 'coerce') 
+
+# Get year of account opened
+banking['acct_year'] = banking['account_opened'].dt.strftime('%Y')
+
+# Print acct_year
+print(banking['acct_year'])

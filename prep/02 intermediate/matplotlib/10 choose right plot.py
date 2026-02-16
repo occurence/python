974b5b71801cd5e146d92cf11bdfb9a3b27bc4a2
Arctaea <joycacao@gmail.com>
2026-02-16 19:15:03 +0800

    Prep: 02 Intermediate Python Matplotlib

---
+++ prep/02 intermediate/matplotlib/10 choose right plot.py
@@ -0,0 +1,10 @@
+"""
+Choose the right plot (2)
+You're a professor in Data Analytics with Python, and you want to visually assess if longer answers on exam questions lead to higher grades. Which plot do you use?
+"""
+
+# Possible answers
+
+# Line plot
+# Scatter plot # Correct
+# Histogram

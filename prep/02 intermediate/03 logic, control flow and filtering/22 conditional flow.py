2d74e3da25f00832ee8c2eb82e005cc026ec5de5
Arctaea <joycacao@gmail.com>
2026-02-24 14:20:53 +0800

    Prep: 02 Intermediate Python Logic, Control Flow and Filtering

---
+++ prep/02 intermediate/03 logic, control flow and filtering/22 conditional flow.py
@@ -0,0 +1,20 @@
+"""
+Warmup
+To experiment with if and else a bit, have a look at this code sample:
+
+area = 10.0
+if(area < 9) :
+    print("small")
+elif(area < 12) :
+    print("medium")
+else :
+    print("large")
+What will the output be if you run this piece of code?
+"""
+
+# Possible answers
+
+# small
+# medium # Correct
+# large
+# The syntax is incorrect; this code will produce an error.
